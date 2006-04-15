3b9ef54395e01b09f90dbada6fa121945316c095
jsalmon3 <>
2006-04-15 00:32:17 +0000

    Removed unneeded files

--- cria_dirs.sh
+++
@@ -1,5 +0,0 @@
-mkdir ./data/sounds/zerg/units/$1
-mkdir ./data/sounds/zerg/units/$1/acknowledgement
-mkdir ./data/sounds/zerg/units/$1/death
-mkdir ./data/sounds/zerg/units/$1/pissed
-mkdir ./data/sounds/zerg/units/$1/selected
