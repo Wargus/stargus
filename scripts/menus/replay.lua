057dc97e7ecf2aaa111b5ccdf838b232b5a76a69
jsalmon3 <>
2007-02-16 05:21:43 +0000

    Updated replay menus

--- scripts/menus/replay.lua
+++ scripts/menus/replay.lua
@@ -1,17 +1,16 @@
 function RunReplayGameMenu()
-  local menu = WarMenu(nil, panel(5), false)
-  menu:setSize(352, 352)
-  menu:setPosition((Video.Width - 352) / 2, (Video.Height - 352) / 2)
-  menu:setDrawMenusUnder(true)
+  local menu = WarMenu(nil, panel("384x256"), false)
+  menu:setSize(384, 256)
+  menu:setPosition((Video.Width - 384) / 2, (Video.Height - 256) / 2)
 
-  menu:addLabel("Select Game", 352 / 2, 11)
+  menu:addLabel("Select Game", 384/2, 8)
 
   local browser = menu:addBrowser("~logs/", "%.log%.?g?z?$",
-    (352 - 18 - 288) / 2, 11 + 98, 306, 108)
+    32, 44, 320, 146)
 
-  local reveal = menu:addCheckBox("Reveal Map", 23, 264, function() end)
+  local reveal = menu:addCheckBox("Reveal Map", 32, 196, function() end)
 
-  menu:addHalfButton("~!OK", "o", 48, 308,
+  menu:addFullLeftButton("~!OK", "o", 20, 216,
     function()
       if (browser:getSelected() < 0) then
         return
@@ -22,29 +21,30 @@ function RunReplayGameMenu()
       SetPlayerData(GetThisPlayer(), "RaceName", "orc")
       menu:stop()
     end)
-  menu:addHalfButton("~!Cancel", "c", 198, 308, function() menu:stop() end)
+  menu:addHalfRightButton("~!Cancel", "c", 260, 216,
+    function() menu:stop() end)
 
   menu:run()
 end
 
 function RunSaveReplayMenu()
-  local menu = WarGameMenu(panel(3))
+  local menu = WarGameMenu(panel("384x256"))
   menu:setSize(384, 256)
   menu:setPosition((Video.Width - 384) / 2, (Video.Height - 256) / 2)
 
-  menu:addLabel("Save Replay", 384 / 2, 11)
+  menu:addLabel("Save Replay", 384/2, 8)
 
   local t = menu:addTextInputField("game.log",
-    (384 - 300 - 18) / 2, 11 + 36, 318)
+    32, 22, 320)
 
   local browser = menu:addBrowser("~logs", ".log$",
-    (384 - 300 - 18) / 2, 11 + 36 + 22, 318, 126)
+    32, 44, 320, 146)
   local function cb(s)
     t:setText(browser:getSelectedItem())
   end
   browser:setActionCallback(cb)
 
-  menu:addHalfButton("~!Save", "s", 1 * (384 / 3) - 106 - 10, 256 - 16 - 27,
+  menu:addFullLeftButton("~!Save", "s", 20, 216,
     -- FIXME: use a confirm menu if the file exists already
     function()
       local name = t:getText()
@@ -64,7 +64,7 @@ function RunSaveReplayMenu()
       menu:stop()
     end)
 
-  menu:addHalfButton("~!Cancel", "c", 3 * (384 / 3) - 106 - 10, 256 - 16 - 27,
+  menu:addHalfRightButton("~!Cancel", "c", 260, 216,
     function() menu:stop() end)
 
   menu:run()
