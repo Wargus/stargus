bf8052cdc30161a9b8e445c9268a6e37129e2810
jsalmon3 <>
2007-10-16 16:06:06 +0000

    Protoss icons

--- scripts/protoss/icons.lua
+++ scripts/protoss/icons.lua
@@ -31,25 +31,48 @@
 
 
 local protoss_icons = {
-  {"icon-protoss-nexus", 154},
+  {"icon-protoss-corsair", 60},
+  {"icon-protoss-dark-templar", 61},
+  {"icon-protoss-dark-archon", 63},
   {"icon-protoss-probe", 64},
   {"icon-protoss-zealot", 65},
-  {"icon-protoss-pylon", 40},
   {"icon-protoss-dragoon", 66},
-  {"icon-protoss-gateway", 43},
-  {"icon-protoss-scourge", 47},
-  {"icon-protoss-queen", 45},
-  {"icon-protoss-ultralisk", 48},
-  {"icon-protoss-defiler", 46},
-  {"icon-protoss-hatchery", 131},
-  {"icon-protoss-extractor", 160},
-  {"icon-protoss-spawning-pool", 142},
-  {"icon-protoss-drone-scv", 7},
-  {"icon-protoss-hatchery", 115},
-
+  {"icon-protoss-high-templar", 67},
+  {"icon-protoss-archon", 68},
+  {"icon-protoss-shuttle", 69},
+  {"icon-protoss-scout", 70},
+  {"icon-protoss-arbiter", 71},
+  {"icon-protoss-carrier", 72},
+  {"icon-protoss-interceptor", 73},
+  {"icon-protoss-dark-templar-hero", 74},
+  {"icon-protoss-zeratul-dark-templar", 75},
+  {"icon-protoss-tassadar-zeratul-archon", 76},
+  {"icon-protoss-fenix-zealot", 77},
+  {"icon-protoss-fenix-dragoon", 78},
+  {"icon-protoss-tassadar-templar", 79},
+  {"icon-protoss-reaver", 83},
+  {"icon-protoss-observer", 84},
+  {"icon-protoss-scarab", 85},
+  {"icon-protoss-nexus", 154},
+  {"icon-protoss-robotics-facility", 155},
+  {"icon-protoss-pylon", 156},
+  {"icon-protoss-assimilator", 157},
+  {"icon-protoss-observatory", 159},
+  {"icon-protoss-gateway", 160},
+  {"icon-protoss-photon-cannon", 162},
+  {"icon-protoss-citadel-of-adun", 163},
+  {"icon-protoss-cybernetics-core", 164},
+  {"icon-protoss-templar-archives", 165},
+  {"icon-protoss-forge", 166},
+  {"icon-protoss-stargate", 167},
+  {"icon-protoss-fleet-beacon", 169},
+  {"icon-protoss-arbiter-tribunal", 170},
+  {"icon-protoss-robotics-support-bay", 171},
+  {"icon-protoss-shield-battery", 172},
+  {"icon-protoss-temple", 174},
 }
 
-pos = table.getn(icons)+1;
+local pos = table.getn(icons)+1;
 
 for i = 1,table.getn(protoss_icons) do
 	icons[pos] = protoss_icons[i]
