f65f66cc945d843d4134495cf62176d6263745be
acidFlamme <>
2007-10-19 21:55:47 +0000

    Re-modeled sound directory, finished all zerg unit/advisor sounds. Added most spell sounds

--- scripts/zerg/sound.lua
+++ scripts/zerg/sound.lua
@@ -1,9 +1,15 @@
--- Basic Zerg sounds
+-- Advisor
 
-MakeSound("zerg-base-attacked", "zerg/units/advisor/upd00.wav")
-MakeSound("zerg-units-attacked", "zerg/units/advisor/upd01.wav")
+MakeSound("zerg-base-attacked", "zerg/units/advisor/base-attacked.wav")
+MakeSound("zerg-units-attacked", "zerg/units/advisor/units-attacked.wav")
+MakeSound("zerg-require-more-minerals", "zerg/units/advisor/require-minerals.wav")
+MakeSound("zerg-require-more-minerals", "zerg/units/advisor/require-gas.wav")
+MakeSound("zerg-spawn-more-overlords", "zerg/units/advisor/spawn-overlords.wav")
+MakeSound("zerg-require-more-energy", "zerg/units/advisor/not-enough-energy.wav")
+MakeSound("zerg-evolution-complete", "zerg/units/advisor/evolution-complete.wav")
+MakeSound("zerg-nuclear-luanch-detected", "zerg/units/advisor/nuclear-launch.wav")
 
--- Zerg Units
+-- drone
 
 MakeSound("zerg-drone-ready", "zerg/units/drone/ready.wav")
 MakeSound("zerg-drone-death", "zerg/units/drone/death/1.wav")
@@ -123,27 +129,250 @@ MakeSound("zerg-overlord-acknowledgement",
 MakeSound("zerg-overlord-death", "zerg/units/overlord/death/1.wav")
 MakeSound("zerg-overlord-ready", "zerg/units/overlord/ready.wav")
 
+--Broodling
+
+MakeSound("zerg-broodling-selected",
+  {"zerg/units/broodling/selected/1.wav",
+  "zerg/units/broodling/selected/2.wav",
+  "zerg/units/broodling/selected/3.wav",
+  "zerg/units/broodling/selected/4.wav"})
+
+MakeSound("zerg-broodling-pissed",
+  {"zerg/units/broodling/pissed/1.wav",
+  "zerg/units/broodling/pissed/2.wav",
+  "zerg/units/broodling/pissed/3.wav",
+  "zerg/units/broodling/pissed/4.wav"})
+
+MakeSound("zerg-broodling-acknowledgement",
+  {"zerg/units/broodling/acknowledgement/1.wav",
+  "zerg/units/broodling/acknowledgement/2.wav",
+  "zerg/units/broodling/acknowledgement/3.wav",
+  "zerg/units/broodling/acknowledgement/4.wav"})
+
+MakeSound("zerg-broodling-death", "zerg/units/broodling/death/1.wav")
+MakeSound("zerg-broodling-ready", "zerg/units/broodling/ready.wav")
+
+
+--defiler
+
+MakeSound("zerg-defiler-selected",
+  {"zerg/units/defiler/selected/1.wav",
+  "zerg/units/defiler/selected/2.wav",
+  "zerg/units/defiler/selected/3.wav",
+  "zerg/units/defiler/selected/4.wav"})
+
+MakeSound("zerg-defiler-pissed",
+  {"zerg/units/defiler/pissed/1.wav",
+  "zerg/units/defiler/pissed/2.wav",
+  "zerg/units/defiler/pissed/3.wav",
+  "zerg/units/defiler/pissed/4.wav"})
+
+MakeSound("zerg-defiler-acknowledgement",
+  {"zerg/units/defiler/acknowledgement/1.wav",
+  "zerg/units/defiler/acknowledgement/2.wav",
+  "zerg/units/defiler/acknowledgement/3.wav",
+  "zerg/units/defiler/acknowledgement/4.wav"})
+
+MakeSound("zerg-defiler-death", "zerg/units/defiler/death/1.wav")
+MakeSound("zerg-defiler-ready", "zerg/units/defiler/ready.wav")
+
+
+--egg
+
+MakeSound("zerg-egg-selected",
+  {"zerg/units/egg/selected/1.wav",
+  "zerg/units/egg/selected/2.wav"})
+
+MakeSound("zerg-egg-pissed",
+  {"zerg/units/egg/pissed/1.wav"})
+
+MakeSound("zerg-egg-death", "zerg/units/egg/death/1.wav")
+
+--guardian
+
+MakeSound("zerg-guardian-selected",
+  {"zerg/units/guardian/selected/1.wav",
+  "zerg/units/guardian/selected/2.wav",
+  "zerg/units/guardian/selected/3.wav",
+  "zerg/units/guardian/selected/4.wav"})
+
+MakeSound("zerg-guardian-pissed",
+  {"zerg/units/guardian/pissed/1.wav",
+  "zerg/units/guardian/pissed/2.wav",
+  "zerg/units/guardian/pissed/3.wav",
+  "zerg/units/guardian/pissed/4.wav"})
+
+MakeSound("zerg-guardian-acknowledgement",
+  {"zerg/units/guardian/acknowledgement/1.wav",
+  "zerg/units/guardian/acknowledgement/2.wav",
+  "zerg/units/guardian/acknowledgement/3.wav",
+  "zerg/units/guardian/acknowledgement/4.wav"})
+
+MakeSound("zerg-guardian-death", "zerg/units/defiler/death/1.wav")
+MakeSound("zerg-guardian-ready", "zerg/units/defiler/ready.wav")
+
+MakeSound("zerg-guardian-attack", "zerg/units/guardian/death/fir00.wav")
+MakeSound("zerg-guardian-hit", "zerg/units/guardian/hit00.wav")
+
+
+--infested terran
+
+MakeSound("zerg-infested-terran-selected",
+  {"zerg/units/infested terran/selected/1.wav",
+  "zerg/units/infested terran/selected/2.wav",
+  "zerg/units/infested terran/selected/3.wav",
+  "zerg/units/infested terran/selected/4.wav"})
+
+MakeSound("zerg-infested-terran-pissed",
+  {"zerg/units/infested terran/pissed/1.wav",
+  "zerg/units/infested terran/pissed/2.wav",
+  "zerg/units/infested terran/pissed/3.wav",
+  "zerg/units/infested terran/pissed/4.wav"})
+
+MakeSound("zerg-infested-terran-acknowledgement",
+  {"zerg/units/infested terran/acknowledgement/1.wav",
+  "zerg/units/infested terran/acknowledgement/2.wav",
+  "zerg/units/infested terran/acknowledgement/3.wav",
+  "zerg/units/infested terran/acknowledgement/4.wav"})
+
+MakeSound("zerg-infested-terran-ready", "zerg/units/infested-terran/ready.wav")
+
+
+--larva
+
+MakeSound("zerg-larva-selected",
+  {"zerg/units/larva/selected/1.wav"})
+
+MakeSound("zerg-larva-pissed",
+  {"zerg/units/larva/pissed/1.wav"})
+
+MakeSound("zerg-larva-death", "zerg/units/larva/death/1.wav")
+MakeSound("zerg-larva-ready", "zerg/units/larva/ready.wav")
+
+
+--mutalisk
+
+MakeSound("zerg-mutalisk-selected",
+  {"zerg/units/mutalisk/selected/1.wav",
+  "zerg/units/mutalisk/selected/2.wav",
+  "zerg/units/mutalisk/selected/3.wav",
+  "zerg/units/mutalisk/selected/4.wav"})
+
+MakeSound("zerg-mutalisk-pissed",
+  {"zerg/units/mutalisk/pissed/1.wav",
+  "zerg/units/mutalisk/pissed/2.wav",
+  "zerg/units/mutalisk/pissed/3.wav",
+  "zerg/units/mutalisk/pissed/4.wav"})
+
+MakeSound("zerg-mutalisk-acknowledgement",
+  {"zerg/units/mutalisk/acknowledgement/1.wav",
+  "zerg/units/mutalisk/acknowledgement/2.wav",
+  "zerg/units/mutalisk/acknowledgement/3.wav",
+  "zerg/units/mutalisk/acknowledgement/4.wav"})
+
+MakeSound("zerg-mutalisk-death", "zerg/units/mutalisk/death/1.wav")
+MakeSound("zerg-mutalisk-ready", "zerg/units/mutalisk/ready.wav")
+MakeSound("zerg-mutalisk-fire", "zerg/units/mutalisk/fir00.wav")
+
+--queen
+
+MakeSound("zerg-queen-selected",
+  {"zerg/units/queen/selected/1.wav",
+  "zerg/units/queen/selected/2.wav",
+  "zerg/units/queen/selected/3.wav",
+  "zerg/units/queen/selected/4.wav"})
+
+MakeSound("zerg-queen-pissed",
+  {"zerg/units/queen/pissed/1.wav",
+  "zerg/units/queen/pissed/2.wav",
+  "zerg/units/queen/pissed/3.wav",
+  "zerg/units/queen/pissed/4.wav"})
+
+MakeSound("zerg-queen-acknowledgement",
+  {"zerg/units/queen/acknowledgement/1.wav",
+  "zerg/units/queen/acknowledgement/2.wav",
+  "zerg/units/queen/acknowledgement/3.wav",
+  "zerg/units/queen/acknowledgement/4.wav"})
+
+MakeSound("zerg-queen-death", "zerg/units/queen/death/1.wav")
+MakeSound("zerg-queen-death-2", "zerg/units/queen/death/2.wav")
+MakeSound("zerg-queen-death-3", "zerg/units/queen/death/3.wav")
+MakeSound("zerg-queen-ready", "zerg/units/queen/ready.wav")
+
+
+--scourge
+
+MakeSound("zerg-scourge-selected",
+  {"zerg/units/scourge/selected/1.wav",
+  "zerg/units/scourge/selected/2.wav"})
+
+MakeSound("zerg-scourge-pissed",
+  {"zerg/units/scourge/pissed/1.wav",
+  "zerg/units/scourge/pissed/2.wav"})
+
+MakeSound("zerg-scourge-acknowledgement",
+  {"zerg/units/scourge/acknowledgement/1.wav",
+  "zerg/units/scourge/acknowledgement/2.wav"})
+
+MakeSound("zerg-scourge-death", "zerg/units/scourge/death/1.wav")
+MakeSound("zerg-scourge-ready", "zerg/units/scourge/ready.wav")
+MakeSound("zerg-scourge-attack", "zerg/units/scourge/att00.wav")
+MakeSound("zerg-scourge-hit", "zerg/units/scourge/hit00.wav")
+
+
+--kerrigan
+
+MakeSound("zerg-kerrigan-selected",
+  {"zerg/units/kerrigan/selected/1.wav",
+  "zerg/units/kerrigan/selected/2.wav",
+  "zerg/units/kerrigan/selected/3.wav",
+  "zerg/units/kerrigan/selected/4.wav"})
+
+MakeSound("zerg-kerrigan-pissed",
+  {"zerg/units/kerrigan/pissed/1.wav",
+  "zerg/units/kerrigan/pissed/2.wav",
+  "zerg/units/kerrigan/pissed/3.wav",
+  "zerg/units/kerrigan/pissed/4.wav"})
+
+MakeSound("zerg-kerrigan-acknowledgement",
+  {"zerg/units/kerrigan/acknowledgement/1.wav",
+  "zerg/units/kerrigan/acknowledgement/2.wav",
+  "zerg/units/kerrigan/acknowledgement/3.wav",
+  "zerg/units/kerrigan/acknowledgement/4.wav"})
+
+MakeSound("zerg-kerrigan-death", "zerg/units/defiler/death/1.wav")
+
+
+
 -- Zerg Buildings
 
-MakeSound("zerg-hatchery-selected", "zerg/units/ha.wav")
+MakeSound("zerg-hatchery-selected", "zerg/units/hatchery-selected.wav")
 
-MakeSound("zerg-building-ready", "zerg/units/chrdy.wav")
+MakeSound("zerg-building-ready", "zerg/units/morph-complete.wav")
 
 
 
-MakeSound("zerg-building-blowup", "zerg/units/bldgdth.wav")
+MakeSound("zerg-building-blowup", "zerg/units/building-death.wav")
 
-MakeSound("zerg-spawning-pool-selected", "zerg/units/ch.wav")
-MakeSound("zerg-spawning-pool-selected", "zerg/units/ch.wav")
-MakeSound("zerg-evolution-chamber-selected", "zerg/units/ev.wav")
-MakeSound("zerg-sunken-colony-selected", "zerg/units/lu.wav")
-MakeSound("zerg-creep-colony-selected", "zerg/units/fc.wav")
-MakeSound("zerg-spore-colony-selected", "zerg/units/sc.wav")
-MakeSound("zerg-extractor-selected", "zerg/units/ig.wav")
-MakeSound("zerg-defiler-mound-selected", "zerg/units/mh.wav")
-MakeSound("zerg-queens-nest-selected-", "zerg/units/ne.wav")
-MakeSound("zerg-nydus-canal-selected", "zerg/units/ny.wav")
-MakeSound("zerg-ultralisk-cavern-selected", "zerg/units/rc.wav")
-MakeSound("zerg-hydralisk-den-selected", "zerg/units/sb.wav")
-MakeSound("zerg-spire-selected", "zerg/units/sp.wav")
-MakeSound("zerg-greater-spire-selected", "zerg/units/mc.wav")
+MakeSound("zerg-spawning-pool-selected", "zerg/units/spawning-pool-selected.wav")
+MakeSound("zerg-evolution-chamber-selected", "zerg/units/evolution-chamber-selected.wav")
+MakeSound("zerg-sunken-colony-selected", "zerg/units/sunken-colony-selected.wav")
+MakeSound("zerg-creep-colony-selected", "zerg/units/creep-colony-selected.wav")
+MakeSound("zerg-spore-colony-selected", "zerg/units/spore-colony-selected.wav")
+MakeSound("zerg-extractor-selected", "zerg/units/extractor-selected.wav")
+MakeSound("zerg-defiler-mound-selected", "zerg/units/defiler-mound-selected.wav")
+MakeSound("zerg-queens-nest-selected", "zerg/units/queens-nest-selected.wav")
+MakeSound("zerg-nydus-canal-selected", "zerg/units/nydus-canal-selected.wav")
+MakeSound("zerg-ultralisk-cavern-selected", "zerg/units/ultralisk-cavern-selected.wav")
+MakeSound("zerg-hydralisk-den-selected", "zerg/units/hydralisk-den-selected.wav")
+MakeSound("zerg-spire-selected", "zerg/units/spire-selected.wav")
+MakeSound("zerg-greater-spire-selected", "zerg/units/greater-spire-selected.wav")
+
+--Spells
+
+MakeSound("spell-consume", "zerg/spells/defiler/consume.wav")
+MakeSound("spell-plague", "zerg/spells/defiler/plague-cast.wav")
+MakeSound("spell-ensnare", "zerg/spells/defiler/ensnare-cast.wav")
+MakeSound("spell-parasite", "zerg/spells/defiler/parasite-cast.wav")
+
+
