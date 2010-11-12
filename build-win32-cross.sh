a3937c15864824fe0bd15aaaf6e357e7a3b605d8
Pali Rohár <pali.rohar@gmail.com>
2010-11-12 15:22:47 +0100

    Fixed build-win32-cross.sh for building 32bit binaries in 64bit mode

--- build-win32-cross.sh
+++ build-win32-cross.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-make clean win32 win32-strip win32-installer CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ WINDRES=x86_64-w64-mingw32-windres STRIP=x86_64-w64-mingw32-strip LDFLAGS=-static NSISFLAGS=-DAMD64
-make clean win32 win32-strip win32-pack win32-installer CC=i686-w64-mingw32-gcc CXX=i686-w64-mingw32-g++ WINDRES=i686-w64-mingw32-windres STRIP=i686-w64-mingw32-strip LDFLAGS=-static
+make clean
+make startool.exe scmconvert.exe CC=i686-w64-mingw32-gcc CXX=i686-w64-mingw32-g++ ARCH32="" LDFLAGS+=-static
+i686-w64-mingw32-strip startool.exe scmconvert.exe
+make stargus.exe CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ WINDRES=x86_64-w64-mingw32-windres ARCH32="" LDFLAGS+=-static
+x86_64-w64-mingw32-strip stargus.exe
+make win32-installer NSISFLAGS=-DAMD64
+make clean win32 win32-strip win32-pack win32-installer CC=i686-w64-mingw32-gcc CXX=i686-w64-mingw32-g++ WINDRES=i686-w64-mingw32-windres STRIP=i686-w64-mingw32-strip LDFLAGS+=-static
