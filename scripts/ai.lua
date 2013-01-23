ab2a247e1868e75f2e36f3d85d9bb2e7d8d57f86
Joris <joris.dauphin@gmail.com>
2013-01-23 13:08:07 +0100

    Remove ignored code inside DefineAiHelper (These relation are filled according to button).

--- scripts/ai.lua
+++ scripts/ai.lua
@@ -10,7 +10,7 @@
 --
 --      ai.lua - Define the AI.
 --
---      (c) Copyright 2007 by Jimmy Salmon
+--      (c) Copyright 2007-2013 by Jimmy Salmon, and Joris Dauphin
 --
 --      This program is free software; you can redistribute it and/or modify
 --      it under the terms of the GNU General Public License as published by
@@ -28,84 +28,26 @@
 --
 --      $Id$
 
-
 race1 = "terran"
 race2 = "zerg"
 race3 = "neutral"
 race4 = "protoss"
 
-
---=============================================================================
---
---  AI helper table, the AI must know where to build units,
---  where to research spells, where to upgrade units.
---  If this is allowed and which dependencies exists, isn't
---  handled here. (see upgrade.lua)
---
---  NOTE: perhaps this could later be used to build the buttons?
---
---  DefineAiHelper(list)
---
-
 --;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 --  * Race terran.
 --;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-DefineAiHelper(
-  {"build", "unit-terran-scv",
-  "unit-terran-academy", "unit-terran-armory", "unit-terran-barracks",
-  "unit-terran-bunker", "unit-terran-command-center", "unit-terran-engineering-bay",
-  "unit-terran-factory", "unit-terran-missile-turret", "unit-terran-refinery",
-  "unit-terran-science-facility", "unit-terran-starport", "unit-terran-supply-depot"},
-
-  {"train", "unit-terran-barracks", "unit-terran-marine", "unit-terran-firebat", "unit-terran-ghost"},
-  {"train", "unit-terran-command-center", "unit-terran-scv"},
-  {"train", "unit-terran-factory", "unit-terran-vulture", "unit-terran-siege-tank", "unit-terran-goliath"},
-  {"train", "unit-terran-starport", "unit-terran-wraith", "unit-terran-dropship", "unit-terran-science-vessel", "unit-terran-battlecruiser"},
-
-  {"research", "unit-terran-academy", "upgrade-terran-u238-shells", "upgrade-terran-stim-pack"},
-  {"research", "unit-terran-engineering-bay",
-  "upgrade-terran-infantry-weapons1", "upgrade-terran-infantry-weapons2", "upgrade-terran-infantry-weapons3",
-  "upgrade-terran-infantry-armor1", "upgrade-terran-infantry-armor2", "upgrade-terran-infantry-armor3"},
-
-  {"repair", "unit-terran-scv",
-  "unit-terran-academy", "unit-terran-armory", "unit-terran-barracks",
-  "unit-terran-bunker", "unit-terran-command-center", "unit-terran-engineering-bay",
-  "unit-terran-factory", "unit-terran-missile-turret", "unit-terran-refinery",
-  "unit-terran-science-facility", "unit-terran-starport", "unit-terran-supply-depot"},
-
-  {"unit-limit", "unit-terran-supply-depot", "food"},
-  {"unit-limit", "unit-terran-command-center", "food"}
-)
-
+DefineAiHelper()
 
 --;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 --  * Race Zerg.
 --;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-
-DefineAiHelper(
-  {"build", "unit-zerg-drone",
-  "unit-zerg-hatchery", "unit-zerg-spawning-pool", "unit-zerg-hydralisk-den",
-  "unit-zerg-creep-colony", "unit-zerg-evolution-chamber", "unit-zerg-extractor",
-  "unit-zerg-defiler-mound", "unit-zerg-nydus-canal", "unit-zerg-queens-nest",
-  "unit-zerg-spire", "unit-zerg-ultralisk-cavern"},
-
-  {"train", "unit-zerg-hatchery", "unit-zerg-zergling", "unit-zerg-drone", "unit-zerg-hydralisk", "unit-zerg-overlord", "unit-zerg-ultralisk", "unit-zerg-mutalisk", "unit-zerg-queen", "unit-zerg-defiler", "unit-zerg-scourge"},
-
-  {"research", "unit-zerg-evolution-chamber", "upgrade-zerg-melee-attacks1", "upgrade-zerg-melee-attacks2", "upgrade-zerg-melee-attacks3", "upgrade-zerg-missle-attacks1", "upgrade-zerg-missle-attacks2", "upgrade-zerg-missle-attacks3", "upgrade-zerg-carapace1", "upgrade-zerg-carapace2", "upgrade-zerg-carapace3"},
-  {"research", "unit-zerg-spire",
-  "upgrade-zerg-flyer-attacks1", "upgrade-zerg-flyer-attacks2", "upgrade-zerg-flyer-attacks3",
-  "upgrade-zerg-flyer-carapace1", "upgrade-zerg-flyer-carapace2", "upgrade-zerg-flyer-carapace3"},
-  {"research", "unit-zerg-greater-spire",
-  "upgrade-zerg-flyer-attacks1", "upgrade-zerg-flyer-attacks2", "upgrade-zerg-flyer-attacks3",
-  "upgrade-zerg-flyer-carapace1", "upgrade-zerg-flyer-carapace2", "upgrade-zerg-flyer-carapace3"},
-
-  {"unit-limit", "unit-zerg-overlord", "food"}
-)
+DefineAiHelper()
 
 Load("scripts/ai/terran.lua")
 Load("scripts/ai/zerg.lua")
+
 function AiPassive()
 end
 
