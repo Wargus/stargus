a71b1f9c2cb19483cda9d03af441946657e4841a
Pali Rohár <pali.rohar@gmail.com>
2013-02-08 20:10:58 +0100

    Remove $id$

--- scripts/anim.lua
+++ scripts/anim.lua
@@ -27,8 +27,6 @@
 --      along with this program; if not, write to the Free Software
 --      Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 --
---      $Id$
-
 
 UnitStill = {"frame 0", "wait 4", "random-goto 99 no-rotate", "random-rotate 1", "label no-rotate", "wait 1",}
 BuildingStill = {"frame 0", "wait 4", "frame 0", "wait 1",}
--- scripts/fonts.lua
+++ scripts/fonts.lua
@@ -26,7 +26,6 @@
 --      along with this program; if not, write to the Free Software
 --      Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 --
---      $Id$
 
 CFont:New("small", CGraphic:New("ui/fonts/font8.png", 8, 11))
 CFont:New("game", CGraphic:New("ui/fonts/font10.png",10, 13))
