853db4c0093855c6c5b7bc8cb68319f2915e51ea
Andreas Volz <andreas.volz@tux-style.com>
2022-04-01 18:43:51 +0200

    add first basic unit test

--- test/module/StormTest.h
+++ test/module/StormTest.h
@@ -4,26 +4,29 @@
 #include <cppunit/TestFixture.h>
 #include <cppunit/extensions/HelperMacros.h>
 
-// STD
+// system
 #include <string.h>
+
+// project
+#include "Storm.h"
 
 class StormTest : public CPPUNIT_NS::TestFixture
 {
-  CPPUNIT_TEST_SUITE (StormTest);
+  CPPUNIT_TEST_SUITE(StormTest);
 
-  CPPUNIT_TEST (testFunc1);
+  CPPUNIT_TEST(StormTest_test1_mpq_txt_extractMemory);
 
-  CPPUNIT_TEST_SUITE_END ();
+  CPPUNIT_TEST_SUITE_END();
 
 public:
-  void setUp ();
-  void tearDown ();
+  void setUp();
+  void tearDown();
 
 protected:
   /**
-   * dummy
+   * Test the extractMemory() function by reading a string from a example .mpq file and compare the content
    */
-  void testFunc1 ();
+  void StormTest_test1_mpq_txt_extractMemory();
 
 private:
 
