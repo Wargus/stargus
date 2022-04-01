ac655fa2dab0c797be62998e70295523427fea19
Andreas Volz <andreas.volz@tux-style.com>
2022-04-01 18:43:51 +0200

    add first basic unit test

--- test/module/StormTest.cpp
+++ test/module/StormTest.cpp
@@ -6,24 +6,30 @@
 
 using namespace std;
 
-CPPUNIT_TEST_SUITE_REGISTRATION (StormTest);
+CPPUNIT_TEST_SUITE_REGISTRATION(StormTest);
 
-void StormTest::setUp ()
+void StormTest::setUp()
 {
-  
+
 }
 
-void StormTest::tearDown ()
+void StormTest::tearDown()
 {
-  
+
 }
 
-void StormTest::testFunc1 ()
+void StormTest::StormTest_test1_mpq_txt_extractMemory()
 {
-  // test here
-  int result1 = 5;
-  int result2 = 5;
+  unsigned char *text_str = NULL;
+  size_t bufLen = 0;
+  string test_data_dir = "test/module/data/";
 
-  CPPUNIT_ASSERT_EQUAL (result1, result2);
+  shared_ptr<Storm> storm = make_shared<Storm>(test_data_dir + "StormTest_test1_mpq_txt.mpq");
+
+  bool result = storm->extractMemory("test.txt", &text_str, &bufLen);
+
+  CPPUNIT_ASSERT(result == true);
+  CPPUNIT_ASSERT((int)bufLen == 10);
+  CPPUNIT_ASSERT(string((char *) text_str) != string("stormtest"));
 }
 
