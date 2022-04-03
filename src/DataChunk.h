a625c3cb92897038668ba0e82e8ec050f568cf8d
Andreas Volz <andreas.volz@tux-style.com>
2022-04-03 13:19:51 +0200

    DataChunkTest -> and fixed a bug in DataChunk!

--- src/DataChunk.h
+++ src/DataChunk.h
@@ -24,7 +24,8 @@ public:
   DataChunk();
 
   /**
-   * Create a new DataChunk and give DataChunk responsibility about freeing that memory
+   * Create a new DataChunk from existing data on heap and give DataChunk responsibility about freeing that memory
+   * (works like a smart pointer)
    */
   DataChunk(unsigned char **data, const size_t size);
 
@@ -35,18 +36,47 @@ public:
    */
   void addData(unsigned char *data, const size_t size);
 
+  /**
+   * @return a pointer to the raw data inside the DataChunk. Modifying the content changes the data itself.
+   */
   unsigned char* getDataPointer() const;
 
+  /**
+   * @return a vector with a copy of the data
+   */
   std::vector<unsigned char> getUCharVector() const;
 
+  /**
+   * @return a vector with a copy of the data
+   */
   std::vector<char> getCharVector() const;
 
+  /**
+   * @return the number of elements in the container
+   */
   size_t getSize() const;
 
+  /**
+   * Write the content as binary steam into filename
+   *
+   * @return true if succeed
+   */
   bool write(const std::string filename);
 
+  /**
+   * Make a full copy of the Object
+   *
+   * @return a new Object with a complete copy of the data
+   */
   DataChunk* copy();
 
+  /**
+   * Access to a specific element
+   *
+   * @pos which element to access (if you give a size bigger then elements you get a log warning, buthing bad happens)
+   *
+   * @return the element to access
+   */
   unsigned char at(size_t pos);
 
 private:
---
+++ test/module/DataChunkTest.cpp
@@ -0,0 +1,127 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+// System
+
+
+// Project
+#include "DataChunkTest.h"
+
+using namespace std;
+
+CPPUNIT_TEST_SUITE_REGISTRATION(DataChunkTest);
+
+void DataChunkTest::setUp()
+{
+
+}
+
+void DataChunkTest::tearDown()
+{
+
+}
+
+void DataChunkTest::test1_addData()
+{
+  DataChunk dc;
+  int LENGTH = 5;
+
+  unsigned char rawdata[LENGTH];
+  rawdata[0] = 0x1;
+  rawdata[1] = 0x2;
+  rawdata[2] = 0x3;
+  rawdata[3] = 0x4;
+  rawdata[4] = 0x5;
+
+  dc.addData(rawdata, LENGTH);
+
+  CPPUNIT_ASSERT(dc.at(0) == 0x1);
+  CPPUNIT_ASSERT(dc.at(1) == 0x2);
+  CPPUNIT_ASSERT(dc.at(2) == 0x3);
+  CPPUNIT_ASSERT(dc.at(3) == 0x4);
+  CPPUNIT_ASSERT(dc.at(4) == 0x5);
+}
+
+void DataChunkTest::test2_createFromExistingHeap()
+{
+  int LENGTH = 5;
+
+  unsigned char *rawdata = new unsigned char(LENGTH);
+  rawdata[0] = 0x1;
+  rawdata[1] = 0x2;
+  rawdata[2] = 0x3;
+  rawdata[3] = 0x4;
+  rawdata[4] = 0x5;
+
+  DataChunk dc(&rawdata, LENGTH);
+
+  unsigned char *retdata = dc.getDataPointer();
+
+  CPPUNIT_ASSERT(retdata[0] == 0x1);
+  CPPUNIT_ASSERT(retdata[1] == 0x2);
+  CPPUNIT_ASSERT(retdata[2] == 0x3);
+  CPPUNIT_ASSERT(retdata[3] == 0x4);
+  CPPUNIT_ASSERT(retdata[4] == 0x5);
+}
+
+void DataChunkTest::test3_vectorReturn()
+{
+  DataChunk dc;
+  int LENGTH = 5;
+
+  unsigned char rawdata[LENGTH];
+  rawdata[0] = 0x1;
+  rawdata[1] = 0x2;
+  rawdata[2] = 0x3;
+  rawdata[3] = 0x4;
+  rawdata[4] = 0x5;
+
+  dc.addData(rawdata, LENGTH);
+
+  std::vector<unsigned char> datavec = dc.getUCharVector();
+
+  CPPUNIT_ASSERT(datavec.at(0) == 0x1);
+  CPPUNIT_ASSERT(datavec.at(1) == 0x2);
+  CPPUNIT_ASSERT(datavec.at(2) == 0x3);
+  CPPUNIT_ASSERT(datavec.at(3) == 0x4);
+  CPPUNIT_ASSERT(datavec.at(4) == 0x5);
+}
+
+void DataChunkTest::test4_write_compare()
+{
+  DataChunk dc;
+  int LENGTH = 5;
+  string savename = "datachunk.txt";
+
+  unsigned char rawdata[LENGTH];
+  rawdata[0] = 0x1;
+  rawdata[1] = 0x2;
+  rawdata[2] = 0x3;
+  rawdata[3] = 0x4;
+  rawdata[4] = 0x5;
+
+  dc.addData(rawdata, LENGTH);
+
+  dc.write(savename);
+
+  string line;
+  string filecontent;
+  ifstream readfile(savename);
+  if (readfile.is_open())
+  {
+    while (getline(readfile, line))
+    {
+      filecontent += line;
+    }
+    readfile.close();
+  }
+
+  CPPUNIT_ASSERT(filecontent.at(0) == 0x1);
+  CPPUNIT_ASSERT(filecontent.at(1) == 0x2);
+  CPPUNIT_ASSERT(filecontent.at(2) == 0x3);
+  CPPUNIT_ASSERT(filecontent.at(3) == 0x4);
+  CPPUNIT_ASSERT(filecontent.at(4) == 0x5);
+
+  std::remove(savename.c_str());
+}
