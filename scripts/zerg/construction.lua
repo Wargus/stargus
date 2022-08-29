2d44256640318668969b34194a07db2943e1085a
Andreas Volz <andreas.volz@tux-style.com>
2022-08-29 16:53:09 +0200

    fix zerg building constructions

--- scripts/zerg/construction.lua
+++ scripts/zerg/construction.lua
@@ -2,11 +2,11 @@
 
 DefineConstruction("construction-zerg", {
   Files = {
-    File = "zerg/units/building morph.png",
-    Size = {160, 192}},
+    File = image_unit_zerg_zbuild_file,
+    Size = image_unit_zerg_zbuild_size},
   ShadowFiles = {
-    File = "zerg/units/building morph shadow.png",
-    Size = {160, 192}},
+    File = image_unit_zerg_zbuild_file,
+    Size = image_unit_zerg_zbuild_size},
   Constructions = {
    {Percent = 0,
     File = "construction",
