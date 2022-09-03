f35827ab243a0f51f0e5b8994e8c90b64c998d1e
Tim Felgentreff <tim.felgentreff@oracle.com>
2022-09-03 21:37:52 +0200

    tweak vulture to see accelleration

--- scripts/terran/unit-terran-vulture.lua
+++ scripts/terran/unit-terran-vulture.lua
@@ -6,12 +6,18 @@ DefineAnimations("animations-terran-vulture", {
   Still = {
     "frame 0", "wait 125",
   },
-  Move = {"unbreakable begin",
-    "move 4", "wait 1", "frame 0", "move 4", "wait 1", "frame 0",
-    "move 4", "wait 1", "frame 0", "move 4", "wait 1", "frame 0",
-    "move 4", "wait 1", "frame 0", "move 4", "wait 1", "frame 0",
-    "move 4", "wait 1", "frame 0", "move 4", "unbreakable end",
-    "wait 1", "frame 0",},
+  Move = {
+    "unbreakable begin", -- accellerate
+      "frame 0", "move 1", "wait 1", "frame 0", "move 1", "wait 1",
+      "frame 0", "move 1", "wait 1", "frame 0", "move 1", "wait 1",
+      "frame 0", "move 2", "wait 1", "frame 0", "move 2", "wait 1",
+    "unbreakable end", "wait 1",
+    "unbreakable begin", -- accellerate 2
+      "frame 0", "move 2", "wait 1", "frame 0", "move 3", "wait 1", "frame 0", "move 3",
+    "unbreakable end", "wait 1",
+    "label fullspeed", "unbreakable begin", "frame 0", "move 4", "wait 1", "frame 0", "move 4", "unbreakable end", "wait 1",
+    "goto fullspeed",
+  },
   Attack = {
     "unbreakable begin", "attack",
     "unbreakable end", "wait 1",
@@ -29,9 +35,9 @@ DefineUnitType("unit-terran-vulture", {
   Costs = {"time", 60, "minerals", 75},
   Speed = 10, NumDirections = 32,
   DrawLevel = 40,
-  TileSize = {1, 1}, BoxSize = {31, 31},
+  TileSize = {5, 5}, BoxSize = {47, 47}, Offset = {1, -2}, BoxOffset = {1, -2},
   BoardSize = 2,
-  SightRange = 4, ComputerReactionRange = 6, PersonReactionRange = 4,
+  SightRange = 16, ComputerReactionRange = 6, PersonReactionRange = 4,
   Armor = 2, BasicDamage = 6, PiercingDamage = 3, Missile = "missile-terran-vulture-grenade",
   AnnoyComputerFactor = 55,
   Priority = 60,
