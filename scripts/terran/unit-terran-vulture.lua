f3976e85c9a36b3b7b01cd6775b25d40c8840254
Tim Felgentreff <tim.felgentreff@oracle.com>
2022-09-07 19:55:20 +0200

    demonstrate with the vulture accelleration, decelleration, and slow turns

--- scripts/terran/unit-terran-vulture.lua
+++ scripts/terran/unit-terran-vulture.lua
@@ -7,16 +7,60 @@ DefineAnimations("animations-terran-vulture", {
     "frame 0", "wait 125",
   },
   Move = {
-    "unbreakable begin", -- accellerate
+    "label accel1", "unbreakable begin",
       "frame 0", "move 1", "wait 1", "frame 0", "move 1", "wait 1",
       "frame 0", "move 1", "wait 1", "frame 0", "move 1", "wait 1",
       "frame 0", "move 2", "wait 1", "frame 0", "move 2", "wait 1",
     "unbreakable end", "wait 1",
-    "unbreakable begin", -- accellerate 2
+      "if-var R >= 80 accel1", -- 180 turn, accellerate from slow
+      "if-var R <= -80 accel1", -- 180 turn, accellerate from slow
+      "if-var W < 2 accel1", -- less than 4 tiles remaining on path, slow down
+    
+    "label accel2", "unbreakable begin",
       "frame 0", "move 2", "wait 1", "frame 0", "move 3", "wait 1", "frame 0", "move 3",
     "unbreakable end", "wait 1",
-    "label fullspeed", "unbreakable begin", "frame 0", "move 4", "wait 1", "frame 0", "move 4", "unbreakable end", "wait 1",
-    "goto fullspeed",
+      "if-var R >= 80 accel1", -- 180 turn, accellerate from slow
+      "if-var R <= -80 accel1", -- 180 turn, accellerate from slow
+      "if-var R >= 20 accel2", -- 90 turn, accellerate from med
+      "if-var R <= -20 accel2", -- 90 turn, accellerate from med
+      "lua-callback print W",
+      "if-var W < 4 accel2", -- less than 8 tiles remaining on path, slow down
+      "if-var W < 2 accel1", -- less than 4 tiles remaining on path, slow down
+
+    "label fullspeed",
+      "unbreakable begin", "frame 0", "move 4", "wait 1", "move 4", "unbreakable end", "wait 1",
+      "if-var R >= 80 accel1", -- 180 turn, accellerate from slow
+      "if-var R <= -80 accel1", -- 180 turn, accellerate from slow
+      "if-var R >= 20 accel2", -- 90 turn, accellerate from med
+      "if-var R <= -20 accel2", -- 90 turn, accellerate from med
+      "lua-callback print W",
+      "if-var W < 4 accel2", -- less than 8 tiles remaining on path, slow down
+      "if-var W < 2 accel1", -- less than 4 tiles remaining on path, slow down
+      "unbreakable begin", "frame 0", "move 2", "wait 1", "move 2", "wait 1", "move 4", "unbreakable end", "wait 1",
+      "if-var R >= 80 accel1", -- 180 turn, accellerate from slow
+      "if-var R <= -80 accel1", -- 180 turn, accellerate from slow
+      "if-var R >= 20 accel2", -- 90 turn, accellerate from med
+      "if-var R <= -20 accel2", -- 90 turn, accellerate from med
+      "lua-callback print W",
+      "if-var W < 4 accel2", -- less than 8 tiles remaining on path, slow down
+      "if-var W < 2 accel1", -- less than 4 tiles remaining on path, slow down
+      "unbreakable begin", "frame 0", "move 4", "wait 1", "move 4", "unbreakable end", "wait 1",
+      "if-var R >= 80 accel1", -- 180 turn, accellerate from slow
+      "if-var R <= -80 accel1", -- 180 turn, accellerate from slow
+      "if-var R >= 20 accel2", -- 90 turn, accellerate from med
+      "if-var R <= -20 accel2", -- 90 turn, accellerate from med
+      "lua-callback print W",
+      "if-var W < 4 accel2", -- less than 8 tiles remaining on path, slow down
+      "if-var W < 2 accel1", -- less than 4 tiles remaining on path, slow down
+      "unbreakable begin", "frame 0", "move 2", "wait 1", "move 2", "wait 1", "move 4", "unbreakable end", "wait 1",
+      "if-var R >= 80 accel1", -- 180 turn, accellerate from slow
+      "if-var R <= -80 accel1", -- 180 turn, accellerate from slow
+      "if-var R >= 20 accel2", -- 90 turn, accellerate from med
+      "if-var R <= -20 accel2", -- 90 turn, accellerate from med
+      "lua-callback print W",
+      "if-var W < 4 accel2", -- less than 8 tiles remaining on path, slow down
+      "if-var W < 2 accel1", -- less than 4 tiles remaining on path, slow down
+      "goto fullspeed",
   },
   Attack = {
     "unbreakable begin", "attack",
@@ -35,6 +79,7 @@ DefineUnitType("unit-terran-vulture", {
   Costs = {"time", 60, "minerals", 75},
   Speed = 10,
   DrawLevel = 40,
+  RotationSpeed = 10,
   -- TileSize = {5, 5}, BoxSize = {47, 47}, Offset = {1, -2}, BoxOffset = {1, -2},
   BoardSize = 2,
   Armor = 2, BasicDamage = 6, PiercingDamage = 3, Missile = "missile-terran-vulture-grenade",
