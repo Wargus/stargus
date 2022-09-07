2d97a1a4f537b9e9922bc2c13e4422a68619acce
Andreas Volz <andreas.volz@tux-style.com>
2022-09-07 14:49:21 +0200

    improve test to analyze broodwar data

--- test/gamedata/dat/DatTest.cpp
+++ test/gamedata/dat/DatTest.cpp
@@ -11,13 +11,144 @@
 #include "FileUtil.h"
 #include "Logger.h"
 
+// as this is 3rd party code I don't fix it ->
+#ifndef _MSC_VER
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wnon-virtual-dtor"
+#endif
+#include "optionparser.h"
+#ifndef _MSC_VER
+#pragma GCC diagnostic pop
+#endif
+// <-
+
 // system
+#include <iostream>
 
 using namespace std;
 
+// some global variables
 static Logger logger("startool.DatTest");
+string dat_files_directory;
 
-int main(int argc, char **argv)
+
+/** option parser **/
+struct Arg: public option::Arg
+{
+  static void printError(const char *msg1, const option::Option &opt, const char *msg2)
+  {
+    fprintf(stderr, "%s", msg1);
+    fwrite(opt.name, opt.namelen, 1, stderr);
+    fprintf(stderr, "%s", msg2);
+  }
+
+  static option::ArgStatus Unknown(const option::Option &option, bool msg)
+  {
+    if (msg)
+      printError("Unknown option '", option, "'\n");
+
+    return option::ARG_ILLEGAL;
+  }
+
+  static option::ArgStatus Required(const option::Option &option, bool msg)
+  {
+    if (option.arg != 0)
+      return option::ARG_OK;
+
+    if (msg)
+      printError("Option '", option, "' requires an argument\n");
+    return option::ARG_ILLEGAL;
+  }
+
+  static option::ArgStatus NonEmpty(const option::Option &option, bool msg)
+  {
+    if (option.arg != 0 && option.arg[0] != 0)
+      return option::ARG_OK;
+
+    if (msg)
+      printError("Option '", option, "' requires a non-empty argument\n");
+    return option::ARG_ILLEGAL;
+  }
+
+  static option::ArgStatus Numeric(const option::Option &option, bool msg)
+  {
+    char *endptr = 0;
+    if (option.arg != 0 && strtol(option.arg, &endptr, 10))
+    {
+    };
+    if (endptr != option.arg && *endptr == 0)
+      return option::ARG_OK;
+
+    if (msg)
+      printError("Option '", option, "' requires a numeric argument\n");
+    return option::ARG_ILLEGAL;
+  }
+};
+
+enum optionIndex
+{
+  UNKNOWN, HELP
+};
+const option::Descriptor usage[] =
+{
+  {
+    UNKNOWN, 0, "", "", option::Arg::None, "USAGE: DatTest <dat_files_directory>\n\n"
+    "Options:"
+  },
+  { HELP, 0, "h", "help", option::Arg::None, "  --help, -h  \t\tPrint usage and exit" },  {
+    UNKNOWN, 0, "", "", option::Arg::None,
+    "\ndat_files_directory \t\tDirectory which contain the extracted stardat.mpq files with arr/*.dat inside...\n"
+
+  },
+  { 0, 0, 0, 0, 0, 0 }
+};
+
+int parseOptions(int argc, const char **argv)
+{
+  argc -= (argc > 0);
+  argv += (argc > 0); // skip program name argv[0] if present
+  option::Stats stats(usage, argc, argv);
+  std::unique_ptr<option::Option[]> options(new option::Option[stats.options_max]), buffer(new option::Option[stats.buffer_max]);
+  option::Parser parse(usage, argc, argv, options.get(), buffer.get());
+
+  if (parse.error())
+    exit(0);
+
+  if (options[HELP])
+  {
+    option::printUsage(std::cout, usage);
+    exit(0);
+  }
+
+  // parse options
+
+  for (option::Option *opt = options[UNKNOWN]; opt; opt = opt->next())
+    std::cout << "Unknown option: " << opt->name << "\n";
+
+  for (int i = 0; i < parse.nonOptionsCount(); ++i)
+  {
+    switch (i)
+    {
+    case 0:
+      cerr << "archive-directory #" << i << ": " << parse.nonOption(i) << "\n";
+      dat_files_directory = parse.nonOption(i);
+      break;
+    default:
+      break;
+    }
+  }
+
+  if (dat_files_directory.empty())
+  {
+    cerr << "Error: 'dat_files_directory' not given!" << endl << endl;
+    option::printUsage(std::cout, usage);
+    exit(1);
+  }
+
+  return 0;
+}
+
+int main(int argc, const char **argv)
 {
 #ifdef HAVE_LOG4CXX
   if (FileExists("logging.prop"))
@@ -30,8 +161,10 @@ int main(int argc, char **argv)
   }
 #endif // HAVE_LOG4CXX
 
-  shared_ptr<Breeze> storm = make_shared<Breeze>("/home/andreas/Downloads/Games/DOS/Starcraft/Original_Backup/starcraft_install.exe_MPQ_stardat.mpq/");
-  //shared_ptr<Breeze> storm = make_shared<Breeze>("/home/andreas/Downloads/Games/DOS/Starcraft/Original_Backup/broodwar_install.exe_MPQ_broodat.mpq/");
+  parseOptions(argc, argv);
+
+
+  shared_ptr<Breeze> storm = make_shared<Breeze>(dat_files_directory);
   dat::DataHub datahub(storm);
 
   dat::CSVExporter csvexporter(datahub);
