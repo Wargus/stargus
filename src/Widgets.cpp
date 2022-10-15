d2e31aa5c4232b31de05631c639de2a14a35a54c
Andreas Volz <andreas.volz@tux-style.com>
2022-10-16 01:52:32 +0200

    implement widget stitching in json

--- src/Widgets.cpp
+++ src/Widgets.cpp
@@ -38,44 +38,47 @@ bool Widgets::convert2(const std::string &arcfile, Storage filename, json &frame
 
   std::vector<char> GrpVec = dcGrp->getCharVector();
 
+  mGRPImage.LoadImage(&GrpVec, true); // true: no duplicate widgets needed
+
+  CheckPath(filename.getFullPath());
+
   if(dcGrp)
   {
     //cout << frameExtractJson << endl; // prints json object to screen
 
-    vector<string> frameNames;
+    //vector<string> frameSingleNames;
 
     for(auto &array : frameExtractJson)
     {
       //int frame_id = array.at("frame");
       string name = array.at("name");
+      nlohmann::json frameArray = array.at("frame");
 
-      //cout << "frame: " << to_string(frame_id) << endl;
-      //cout << "name: " << name << endl;
+      int frameStitching = 0;
+      vector<int> stitchedFrames;
+      for(auto frame : frameArray)
+      {
+        cout << "frame: " << frame << endl;
+        stitchedFrames.push_back(frame);
+        frameStitching++;
+      }
 
-      frameNames.push_back(name);
+      if(frameStitching == 1)
+      {
+        //frameSingleNames.push_back(name);
+        mGRPImage.SaveSinglePNG(filename.getFullPath() + "/" + name, *stitchedFrames.begin(), *stitchedFrames.begin()+1, true);
+      }
+      else if(frameStitching > 1)
+      {
+        mGRPImage.SaveStitchedPNG(filename.getFullPath() + "/" + name, stitchedFrames, 0, true);
+      }
+      else
+      {
+        cerr << "something wrong with frame array!" << endl;
+      }
     }
 
-    mGRPImage.LoadImage(&GrpVec, true); // true: no duplicate widgets needed
-
-    CheckPath(filename.getFullPath());
-    mGRPImage.SaveSinglePNG(filename.getFullPath(), frameNames, 0, mGRPImage.getNumberOfFrames(), true);
-
-    vector<int> testVec;
-    testVec.push_back(88);
-    testVec.push_back(82);
-    testVec.push_back(82);
-    testVec.push_back(82);
-    testVec.push_back(82);
-    testVec.push_back(82);
-    testVec.push_back(82);
-    testVec.push_back(82);
-    testVec.push_back(82);
-    testVec.push_back(82);
-    testVec.push_back(82);
-    testVec.push_back(82);
-    testVec.push_back(83);
-
-    mGRPImage.SaveStitchedPNG(filename.getFullPath()+"/widget2.png", testVec, 0, true);
+    //mGRPImage.SaveSinglePNG(filename.getFullPath(), frameSingleNames, 0, mGRPImage.getNumberOfFrames(), true);
   }
 
   return result;
